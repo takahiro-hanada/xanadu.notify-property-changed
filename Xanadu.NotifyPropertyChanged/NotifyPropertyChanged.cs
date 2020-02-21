ff0f6667d09eaf7ff65102400189d80524327aeb
Takahiro Hanada <43534055+takahiro-hanada@users.noreply.github.com>
2020-02-21 14:18:52 +0900

    Add files.

---
+++ Xanadu.NotifyPropertyChanged/NotifyPropertyChanged.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Xanadu
+{
+    public abstract class NotifyPropertyChanged : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual bool ChangeProperty<T>(ref T oldValue, T newValue, [CallerMemberName]string propertyName = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(oldValue, newValue))
+            {
+                oldValue = newValue;
+
+                OnPropertyChanged(propertyName);
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            PropertyChanged?.Invoke(this, e);
+        }
+    }
+}
